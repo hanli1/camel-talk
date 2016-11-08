39069495d44322ead9e0d32b18c2f2dc5904ca36
Han Li <hl565@cornell.edu>
2016-11-08 01:10:14 -0500

    added specs for renderer

--- renderer.mli
+++ renderer.mli
@@ -1,4 +1,11 @@
-
+(*
+ * render on the command line based on the current state [state] and a list
+ * of channels
+ *)
 val render_channels_list : state -> string list -> unit
 
+(*
+ * render on the command line based on the current state [state] and a list
+ * of messages
+ *)
 val render_channel_messages : state -> message list -> unit
