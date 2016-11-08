ad3e2c5e5f82c2901ab4515e7a103bd5514ca18a
Joshua Ying <jmy48@cornell.edu>
2016-11-08 00:34:03 -0500

    preliminary main.mli

--- main.mli
+++ main.mli
@@ -1,6 +1,21 @@
+include Yojson
+
 type current_state = 
 {
-	current_org = 
-	current_channel =
-	current_user = 
+  current_org : string option;
+  current_channel : string option;
+  current_user : string;
 }
+
+(* Continuously called to refresh for new messages, repaints the terminal 
+to display new current messages *)
+val draw_update : current_state -> unit
+
+(* Prompts the user to enter in username and password information as the
+first thing when the program is run. *)
+val prompt_login : -> string -> string -> bool
+
+val register : string -> string -> bool
+
+(* Contains the REPL *)
+val main : unit
