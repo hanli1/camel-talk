f7af970fba0deabb74e2c56341c7fa5088bbfa18
Han Li <hl565@cornell.edu>
2016-11-25 12:22:30 -0500

    tested simple and reminder, more modifications for ANSI

--- renderer.ml
+++ renderer.ml
@@ -55,8 +55,16 @@ let render_channels_list curr_org resp_obj =
   render_channels_list_helper (get_member_list_of_string resp_obj "team_channels");
   render_channels_list_helper (get_member_list_of_string resp_obj "private_channels")
 
+let get_width _ =
+  fst (ANSITerminal.size())
+let get_height _ =
+  snd (ANSITerminal.size())
+
+let rec gen_line str count =
+  if count = 0 then str
+  else gen_line (str ^ "_") (count - 1)
 let print_linebreak _ =
-  ANSITerminal.(print_string [] "\n___________________________________\n")
+  ANSITerminal.(print_string [] ("\n"^ (gen_line "" (get_width ())) ^"\n"))
 
 let print_newline _ =
   ANSITerminal.(print_string [] "\n")
@@ -70,6 +78,12 @@ let rec print_votes lst =
     ANSITerminal.(print_string [] (choice ^ ": " ^ (string_of_int count) ^ ", "));
     print_votes t
 
+let print_meta_data name time =
+  ANSITerminal.(print_string [green] name);
+  let time_length = String.length time in
+  ANSITerminal.set_cursor (get_width() - time_length) (snd(ANSITerminal.pos_cursor()));
+  ANSITerminal.(print_string [green] time)
+
 let render_message msg =
   let open Yojson.Basic.Util in
   let msg_type = get_member_string msg "message_type" in
@@ -77,22 +91,23 @@ let render_message msg =
   let time_stamp = get_member_string msg "time_stamp" in
   let mess = msg |> member "message" in
   if msg_type = "simple" then begin
-    ANSITerminal.(print_string [] (get_member_string mess "text"));
+    print_meta_data user_id time_stamp;
     print_newline();
-    ANSITerminal.(print_string [green] (user_id ^ "     " ^ time_stamp));
+    ANSITerminal.(print_string [] (get_member_string mess "text"));
     print_linebreak ()
   end
   else if msg_type = "reminder" then begin
-    ANSITerminal.(print_string [] ("Reminder set for " ^ (get_member_string mess "reminder")));
+    print_meta_data user_id time_stamp;
     print_newline();
-    ANSITerminal.(print_string [green] (user_id ^ "     " ^ time_stamp));
+    ANSITerminal.(print_string [] ("Reminder set for " ^ (get_member_string mess "reminder")));
     print_linebreak ()
   end
   else begin
+    print_meta_data user_id time_stamp;
+    print_newline();
     ANSITerminal.(print_string [] (get_member_string mess "pollname"));
     print_newline ();
     print_votes (get_member_list mess "choices");
-    ANSITerminal.(print_string [green] (user_id ^ "     " ^ time_stamp));
     print_linebreak ()
   end
 
